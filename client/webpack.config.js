0d400f1209ba38ca8cf7aa2d8e81f4f334d4ec9a
Michael Reyna <25.27.reyna@gmail.com>
2023-03-28 21:02:02 -0600

    added logic to webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,19 +11,59 @@ module.exports = () => {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js'
+      install: './src/js/install.js',
+      header: './src/js/header.js',
+      editor: './src/js/editor.js',
+      database: './src/js/database.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack-Plugin'
+      }),
+      new WebpackPwaManifest(
+        {
+          filename: "manifest.json",
+          name: "Just Another Text Editor",
+          short_name: "J.A.T.E",
+          description: "Take notes with Text Editor",
+          inject: true,
+          orientation: 'portrait',
+          display: "standalone",
+          fingerprints: false,
+          background_color: '#225ca3',
+          theme_color: '#225ca3',
+          start_url: './',
+          publicPath: "./",
+          icons: {
+            src: path.resolve('./src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], 
+            destination: path.join('assets', 'icons'),
+        }
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        }
       ],
     },
   };
